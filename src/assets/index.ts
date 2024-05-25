aca6d6005722eefc28d74eee21b5210a7475b87b
Apple <apple@MacBook-Pro-Apple.local>
2024-05-25 16:41:07 +0300

    add error props for input

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,11 @@
 import EmailIcon from "./icons/email-icon.svg?react";
+import OpenEye from "./icons/open-eye.svg?react";
+import CloseEye from "./icons/close-eye.svg?react";
+import InputError from "./icons/input-error.svg?react";
 
 export const Icons = {
   EmailIcon,
+  OpenEye,
+  CloseEye,
+  InputError,
 };
---
+++ src/components/UI/Input.tsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import { IconButton, InputAdornment, TextField, styled } from "@mui/material";
+import { Icons } from "../../assets";
+
+interface PROPS {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  type?: "email" | "password" | "text" | "number";
+  placeholder: string;
+  props?: void;
+  error?: boolean;
+}
+
+export const Input: React.FC<PROPS> = ({
+  value,
+  onChange,
+  type = "text",
+  placeholder,
+  error,
+  ...props
+}) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleTogglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <StyledInput
+      type={showPassword ? "text" : type}
+      value={value}
+      placeholder={placeholder}
+      onChange={onChange}
+      error={error}
+      {...props}
+      InputProps={{
+        endAdornment:
+          type === "password" ? (
+            <InputAdornment position="end">
+              <IconButton onClick={handleTogglePasswordVisibility} edge="end">
+                {error ? (
+                  <Icons.InputError />
+                ) : showPassword ? (
+                  <Icons.OpenEye />
+                ) : (
+                  <Icons.CloseEye />
+                )}
+              </IconButton>
+            </InputAdornment>
+          ) : error ? (
+            <InputAdornment position="end">
+              <IconButton onClick={handleTogglePasswordVisibility} edge="end">
+                <Icons.InputError />
+              </IconButton>
+            </InputAdornment>
+          ) : null,
+      }}
+    />
+  );
+};
+
+const StyledInput = styled(TextField)(({ error }) => ({
+  height: "46px",
+  border: `1px solid ${error ? "#F10000" : "#BDBDBD"}`,
+  borderRadius: "6px",
+  paddingLeft: "10px",
+  display: "flex",
+  justifyContent: "center",
+  ".MuiInputBase-input": {
+    fontSize: "12px",
+    fontWeight: "400",
+    color: `${error ? "red" : "#020202"}`,
+  },
+  "::placeholder": {
+    color: `${error ? "red" : "#8D949E"}`,
+  },
+  ":focus-within": {
+    border: `1px solid ${error ? "red" : "#6200EE"}`,
+  },
+  "& fieldset": { border: "none" },
+}));
